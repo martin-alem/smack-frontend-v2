b7dd450bfe27670737bdcb41f2f939f354658d13
Martin Alemajoh <alemajohmartin@gmail.com>
2021-12-10 00:18:06 -0500

    completed the setup2FA component implementation

--- src/pages/setup-2fa/Setup2FA.js
+++ src/pages/setup-2fa/Setup2FA.js
@@ -2,24 +2,35 @@ import React from "react";
 import "./Setup2FA.css";
 import Button from "./../../components/button/Button";
 import TextInput from "./../../components/text-input/TextInput";
+import image from "./../../images/2fa.svg";
 
 function Setup2FA() {
   return (
     <div className="Setup2FA">
-      {/* <Button
-        variant="contained"
-        text="Setup 2FA"
-        action={() => {
-          console.log("I was clicked");
-        }}
-      /> */}
-      <TextInput
-        name="code"
-        placeholder="Verification code"
-        action={() => {
-          console.log("Hi there!");
-        }}
-      />
+      <h1 className="Setup2FA-heading">2-Factor-Auth</h1>
+      <div className="Setup2FA-image-container">
+        <img className="Setup2FA-image" src={image} alt="2 factor authentication" />
+      </div>
+      <p className="Setup2FA-message">Enter the code sent the phone number ending in ***5041</p>
+      <div className="Setup2FA-input">
+        <TextInput
+          name="code"
+          placeholder="Verification code"
+          action={() => {
+            console.log("Hi there!");
+          }}
+        />
+      </div>
+      <div className="Setup2FA-button">
+        <Button variant="contained" text="Setup 2FA" />
+      </div>
+      <div className="Setup2FA-info">
+        <span className="material-icons icon">info</span>
+        <p className="Setup2FA-info-text">
+          Devices added as trusted will not go through the verification process when next you sign
+          in
+        </p>
+      </div>
     </div>
   );
 }
