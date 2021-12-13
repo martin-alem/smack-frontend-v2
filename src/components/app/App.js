867e57a397170500027bca185cd215102e3db331
Martin Alemajoh <alemajohmartin@gmail.com>
2021-12-12 20:16:16 -0500

    setup the structure of the home page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-// import Login from "../../pages/login/Login";
-// import Setup2FA from "../../pages/setup-2fa/Setup2FA";
+import Login from "../../pages/login/Login";
+import Setup2FA from "../../pages/setup-2fa/Setup2FA";
 import Verify from "../../pages/verify/Verify";
+import Home from "../../pages/home/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Home />
       {/* <Login /> */}
       {/* <Setup2FA /> */}
-      <Verify />
+      {/* <Verify /> */}
     </div>
   );
 }
---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="Home">
+      <nav className="Home-nav"></nav>
+      <section className="Home-chats"></section>
+      <section className="Home-chat-area"></section>
+    </div>
+  );
+}
+
+export default Home;
