0bef4716c3c14425c127570988ac3b7200c3f410
Martin Alemajoh <alemajohmartin@gmail.com>
2021-12-17 21:59:43 -0500

    added emoji support

--- src/components/chat_control/ChatControl.js
+++ src/components/chat_control/ChatControl.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./ChatControl.css";
+import Picker from "emoji-picker-react";
 
 function ChatControl(props) {
   const { name } = props;
-
+  const [chosenEmoji, setChosenEmoji] = React.useState(null);
+  const [isEmojiVisible, setIsEmojiVisible] = React.useState(false);
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji(emojiObject);
+  };
   return (
     <div className="ChatControl">
+      <div className="ChatControl-emoji">
+        {isEmojiVisible ? <Picker onEmojiClick={onEmojiClick} /> : null}
+      </div>
       <div className="ChatControl-input">
         <textarea className="ChatControl-textarea" placeholder={`Message ${name}`}></textarea>
         <span className="material-icons-outlined">send</span>
@@ -13,7 +21,14 @@ function ChatControl(props) {
       <div className="ChatControl-actions">
         <span className="material-icons-outlined">photo_camera</span>
         <span className="material-icons-outlined">mic</span>
-        <span className="material-icons-outlined">emoji_emotions</span>
+        <span
+          onClick={() => {
+            setIsEmojiVisible(!isEmojiVisible);
+          }}
+          className="material-icons-outlined"
+        >
+          emoji_emotions
+        </span>
         <span className="material-icons-outlined">attachment</span>
       </div>
     </div>
